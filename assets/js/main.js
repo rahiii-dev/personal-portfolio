016d104cb3552bdd58edb19be70e4aeab6cb6717
rahiii-dev <76057585+rahiii-dev@users.noreply.github.com>
2022-11-29 14:27:39 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,80 @@
+
+// animations
+let hamburgerAnimation = gsap.timeline({paused: true})
+hamburgerAnimation.to('#hamburger .top', {y: 28, ease: 'back', duration:0.3})
+                  .to('#hamburger .bottom', {y: -28, ease: 'back', duration:0.3}, '<')
+                  .to('#hamburger .middle', {opacity: 0})
+                  .to('#hamburger .top', {
+                    x: 7,
+                    y: 33,
+                    rotate: 45,
+                    transformOrigin: 'center', duration: 0.2,
+                    ease: 'back'
+                  })
+                  .to('#hamburger .bottom', {
+                    x: 7,
+                    rotate: -45,
+                    transformOrigin: 'center', duration: 0.2,
+                    ease: 'back'
+                  }, '<')
+                  .to('nav', {x: 0,}, 0.1)
+                  .from('nav .mobile-list li:not(.social-links)', {
+                    x: 50,
+                    opacity: 0,
+                    stagger: 0.2,
+                    ease: 'back()',
+                    duration: 0.7,
+                  })
+                  .from('nav .mobile-list .social-links a', {
+                    y: 10,
+                    scale: 0.5,
+                    opacity: 0,
+                    stagger: {
+                        each: 0.2,
+                        from: 'center'
+                    },
+                    ease: 'back()',
+                    duration: 0.7,
+                  }, '<')
+
+let heroAnimation = gsap.timeline({paused: true})
+
+heroAnimation.from('#Hero-Sec', {opacity:0, duration:1})
+             .from('#Hero-Sec .content-wrapper .col-left > h1 span', {
+              xPercent: -105,
+              stagger: 0.2,
+             })
+             .from('#hamburger', {opacity: 0}, '<')
+             .from('#hamburger .top', {y: 28, ease: 'back', duration:0.3})
+             .from('#hamburger .bottom', {y: -28, ease: 'back', duration:0.3}, '<')
+             .from('#Hero-Sec .content-wrapper .col-left a', {
+              opacity: 0,
+              y: 40,
+             }, '<')
+             .from('#Hero-Sec .content-wrapper .col-right img', {
+              yPercent: 115,
+              ease: 'back',
+             }, 1)
+
+$(document).ready(function () {
+    // init
+    let vh = window.innerHeight * 0.01;
+    $(document.body).css('--vh', `${vh}px`);
+    heroAnimation.play()
+
+
+    //   functions
+
+
+    // events
+    $('#hamburger').click(function (e) { 
+        if($(this).hasClass('active')){
+            hamburgerAnimation.play()
+            $(this).removeClass('active');
+        }
+        else{
+            hamburgerAnimation.reverse()
+            $(this).addClass('active');
+        }
+    });
+});
