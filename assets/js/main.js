0a98f6a60547aedffc687dc00a27eb4d47019003
rahiii-dev <76057585+rahiii-dev@users.noreply.github.com>
2022-12-08 17:46:03 +0530

    added preloader

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,51 @@
+$(window).on("load", function () {
+  setTimeout(function(){
+    $("#loading").hide();
+  }, 500)
+});
+
 $(document).ready(function () {
   // init
   function init() {
     let vh = window.innerHeight * 0.01;
     $(document.body).css("--vh", `${vh}px`);
     heroAnimation.play();
-    dektopNavAnime.play()
-    skillScroll()
-    aboutScroll()
+    dektopNavAnime.play();
+    skillScroll();
+    aboutScroll();
   }
 
-  init()
+  setTimeout(function(){
+    init()
+  }, 550)
 
   //   functions
-  $(window).resize(function () { 
-    console.log("resize")
-    ScrollTrigger.refresh()
+  $(window).resize(function () {
+    ScrollTrigger.refresh();
   });
- 
+
   // events
   $("#hamburger").click(function (e) {
-    let vh = $('#Hero-Sec').height();
+    let vh = $("#Hero-Sec").height();
 
-    $(this).toggleClass('active');
+    $(this).toggleClass("active");
 
     if ($(this).hasClass("active")) {
       hamburgerAnimation.play();
       $(document.body).css({
-        'height': `${vh}px`,
-        'overflow-y': 'hidden'
+        height: `${vh}px`,
+        "overflow-y": "hidden",
       });
     } else {
       hamburgerAnimation.reverse();
       $(document.body).css({
-        'height': `auto`,
-        'overflow-y': 'auto',
+        height: `auto`,
+        "overflow-y": "auto",
       });
     }
   });
 
-  $("#contact .container .content form").submit(function (e) { 
+  $("#contact .container .content form").submit(function (e) {
     e.preventDefault();
-    
   });
-
 });
